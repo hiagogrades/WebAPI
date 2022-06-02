767b5b92ceb41a2f6440a668f9eff68b4b95f24c
Hiago Grades <hiagogrades@hotmail.com>
2022-06-02 14:24:13 -0300

    Adiciona o Controller e configura a Class Startup

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("")]
+}
