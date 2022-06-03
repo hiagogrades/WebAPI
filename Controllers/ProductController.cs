8f288da3e2a731c6fe74cffb3469cc05f8e8b63b
Hiago Grades <hiagogrades@hotmail.com>
2022-06-03 17:33:51 -0300

    Finaliza a classe ProductController

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic; //Biblioteca para usar o List<>
+using System.Linq;
 using System.Threading.Tasks; //Biblioteca para usar os métodos async (Task<>)
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -23,10 +24,41 @@ namespace WebAPI.Controllers
         [Route("{id:int}")] //Adicionando um id na rota. E utilizando|| restrição de rota utilizando o :int, para ser aceito somente números inteiros.
         public async Task<ActionResult<Product>> GetById([FromServices] DataContext context, int id)
         {
-            var product = await context.Products.Include(x => x.Category).FirstOrDefaultAsync(x => x.Id == id);
+            var product = await context.Products.Include(x => x.Category)
+                .AsNoTracking() //Utilizar como boa prática para que o EF não crie proxys das buscas (versões do objeto) ****Utilizar somente para select****
+                .FirstOrDefaultAsync(x => x.Id == id);
             return product;
         }
 
+        [HttpGet]
+        [Route("{categories/id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategory([FromServices] DataContext context, int id)
+        {
+            var products = await context.Products
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .Where(x => x.CategoryId == id)
+                .ToListAsync();
+            return products;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Product>> Post(
+            [FromServices] DataContext context,
+            [FromBody] Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
 
     }
 }
